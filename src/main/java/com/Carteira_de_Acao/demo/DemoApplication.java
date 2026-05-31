fc3800de1e0f7e768e539d4f1cfeed2424057e73
Boni03 <bonigabriel77@gmail.com>
2026-05-30 21:44:27 -0300

    ADD JAVASPRING

---
+++ src/main/java/com/Carteira_de_Acao/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.Carteira_de_Acao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Carteira_de_Acao/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Carteira_de_Acao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
